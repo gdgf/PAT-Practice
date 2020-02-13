13ee56112debd139a45969b6009606a4b48ecd5e
NZQH <794428738@qq.com>
2020-02-13 21:34:50 +0800

    注意数据格式

--- BPAT/Bpat1020.cpp
+++ BPAT/Bpat1020.cpp
@@ -1,52 +1,43 @@
-/*
- * 
- * 贪心算法解决
- * 结构体排序：用内置排序时的解决方案
- *     只需给结构体的头指针和尾指针
- * 还是有部分错误
- */
+//注意数据格式，直接设为double为好
 #include<iostream>
 #include<algorithm>
-#include<stdio.h>
+#include<cstdio>
+
 using namespace std;
 struct MoonCake{
-    int num;
-    int price;
+    double num;
+    double price;
 };
-bool comp(MoonCake cake1,MoonCake cake2){
-    if(((double)cake1.price/cake1.num)>((double)cake2.price/cake2.num)){
-        return true;
-    }else{
-        return false;
-    }
+
+//根据单价由高到低进行排序
+bool cmp(MoonCake cake1,MoonCake cake2){
+    return cake1.price/cake1.num > cake2.price/cake2.num;
 }
 int main(){
     int N,D;
     cin>>N>>D;
-    MoonCake *cake=new MoonCake[N];
+    MoonCake cake[N];
+
     for(int i=0;i<N;i++){
         cin>>cake[i].num;
     }
     for(int i=0;i<N;i++){
         cin>>cake[i].price;
     }
-    sort(cake,cake+N,comp);
-   
-    int sum=0;
-    double cost=0;
+    sort(cake,cake+N,cmp);
+
+    double sum=0,cost=0;
+
     for(int i=0;i<N;i++){
         sum=sum+cake[i].num;
-        if(sum>=D){
-            double temp=(double)cake[i].price*(sum-D)/cake[i].num;
+        if(sum>=D){ //超过
+            double temp=cake[i].price/cake[i].num * (sum-D);  //多算的钱
             cost=cost+cake[i].price-temp;
-           // cout<<cost<<endl;
             break;
-        }else{
+        }else{  //不够
             cost+=cake[i].price;
-            //cout<<cost<<endl;
         }            
     }
-    //cout<<cost<<endl;
     printf("%.2lf",cost);   
     return 0;
 }
