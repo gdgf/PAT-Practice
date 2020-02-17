ef686dd89d27fd119f0ddbacec623a1501512ecc
gdgf <794428738@qq.com>
2020-02-17 12:12:20 +0800

    update 1030

--- BPAT/Bpat1030.cpp
+++ BPAT/Bpat1030.cpp
@@ -5,16 +5,22 @@ int main(){
     int N,count=0;
     double p,array[100010];;
     scanf("%d%lf",&N,&p);
+
     for(int i=0;i<N;i++)
         scanf("%lf",&array[i]);
     sort(array,array+N);
-    for(int i=0;i<N;i++)            //遍历，将a[i]作为最小数
-        for(int j=i+count;j<N;j++){ //j置为要满足可以更新数列长度的值，减少循环次数
-            if(array[j]>array[i]*p) //如果不满足条件了，则将下一个元素最为最小值
-            break;
-            if(j-i+1>count)         //如果此次的长度大于上一次，更新数列长度
+    
+    //长度array[i]~array[j]
+    
+    for(int i=0;i<N;i++){             //遍历，将a[i]作为最小数
+        for(int j=i+count;j<N;j++){   //j置为要满足可以更新数列长度的值，减少循环次数
+            if(array[j]>array[i]*p)   //如果不满足条件了，则将下一个元素最为最小值
+                break;
+
+            if(j-i+1>count)           //如果此次的长度大于上一次，更新数列长度
                 count=j-i+1;
         }
+    }   
     printf("%d",count);
     system("pause");
     return 0;
